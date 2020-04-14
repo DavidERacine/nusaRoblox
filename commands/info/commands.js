73b315340589ac165b7e0d05936cc96b38c3b31f
David E. Racine, III <devGralius@robloxnusa.org>
2020-04-14 06:19:30 -0500

    Updated the -commands/-cmds command to be single embed, or max 25 per time.

--- commands/info/commands.js
+++ commands/info/commands.js
@@ -7,20 +7,26 @@ module.exports = {
     Usage: "",
     Run: async(Service, message, args)=>{
         const Commands = await Service.Client.Commands;
-        let Categories = ["Info","Groups","Verification"];
+        let Categories = ["Info","Groups","Verification","Clan Management","Moderators"];
+        let Fields = [];
         await Service.Utils.asyncForEach(Categories, async(Cat)=>{
             let CatCmds = await Service.Client.Commands.filter(Cmd=>Cmd.Category == Cat);
 
-            let Embeds = [];
             await Service.Utils.asyncForEach(CatCmds.array(), async(Cmd)=>{
-                await Embeds.push({name:`${Service.Settings.Prefix}${Cmd.Name} ${Cmd.Usage}`, value:`${Cmd.Description}`});
+                if (Fields.length == 25){
+                    let Embed = await Service.Utils.BuildEmbed(`nusaRoblox Commands`, `The following are the commands.`, Fields);
+                    Fields = [];
+                    await message.author.send(Embed).then((msg)=>{}).catch(async(err)=>{
+                        return await Service.Utils.ReturnMessage(message, `nusaRoblox Error`,'Privacy error most likely.');                
+                    });
+                }
+                await Fields.push({name:`${Service.Settings.Prefix}${Cmd.Name} ${Cmd.Usage}`, value:`Aliases: ${(Cmd.Aliases.length == 0 ? "None." : Cmd.Aliases.join(','))}\nCategory: ${Cmd.Category}\nDescription: ${Cmd.Description}\nValid: ${(Cmd.Mode != "Both" ? `${Cmd.Mode} only` : `Anywhere.`)}`});
             });
 
-            let CatEmbed = await Service.Utils.BuildEmbed(`nusaRoblox Commands | ${Cat}`, ``, Embeds);
-            await message.author.send(CatEmbed).then((msg)=>{}).catch(async(err)=>{
-                return await Service.Utils.ReturnMessage(message, `nusaRoblox Error`,'Privacy error most likely.');                
-            });
-
+        });
+        let Embed = await Service.Utils.BuildEmbed(`nusaRoblox Commands`, `The following are the commands.`, Fields);
+        await message.author.send(Embed).then((msg)=>{}).catch(async(err)=>{
+            return await Service.Utils.ReturnMessage(message, `nusaRoblox Error`,'Privacy error most likely.');                
         });
         await Service.Utils.ReturnMessage(message, `nusaRoblox Commands`,'A list of commands by category have been direct-messaged to you.');
     }
